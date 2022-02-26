4e4ead31b38ccdc23cf24092ab49bc95f6c52d3f
Remy <liurenmi@msu.edu>
2022-02-26 11:01:40 -0500

    Use pytest cache

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -1,7 +1,5 @@
 import os.path as osp
 import pathlib
-import shutil
-import tempfile
 import unittest
 
 import pandas as pd
@@ -12,10 +10,16 @@ from geneplexus import geneplexus
 
 HOMEDIR = pathlib.Path(__file__).parent.parent
 FILENAMES = [
+    "CorrectionMatrix_DisGeNet_DisGeNet_BioGRID_Embedding.npy",
+    "CorrectionMatrix_DisGeNet_GO_BioGRID_Embedding.npy",
+    "CorrectionMatrix_GO_DisGeNet_BioGRID_Embedding.npy",
     "CorrectionMatrix_GO_GO_BioGRID_Embedding.npy",
+    "CorrectionMatrixOrder_DisGeNet_BioGRID.txt",
     "CorrectionMatrixOrder_GO_BioGRID.txt",
     "Data_Embedding_BioGRID.npy",
     "Edgelist_BioGRID.edg",
+    "GSC_DisGeNet_BioGRID_GoodSets.pickle",
+    "GSC_DisGeNet_BioGRID_universe.txt",
     "GSC_GO_BioGRID_GoodSets.pickle",
     "GSC_GO_BioGRID_universe.txt",
     "IDconversion_Homo-sapiens_ENSG-to-Entrez.pickle",
@@ -29,42 +33,40 @@ FILENAMES = [
     "NodeOrder_GIANT-TN.txt",
     "NodeOrder_STRING-EXP.txt",
     "NodeOrder_STRING.txt",
+    "PreTrainedWeights_DisGeNet_BioGRID_Embedding.pickle",
     "PreTrainedWeights_GO_BioGRID_Embedding.pickle",
 ]
 
 
-class TestGenePlexus(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.tempdir = tempfile.mkdtemp()
+@pytest.fixture(scope="class")
+def data(request):
+    datadir = request.config.cache.makedir("download")
+    geneplexus.download_select_data(
+        datadir,
+        "All",
+        "BioGRID",
+        "Embedding",
+        ["GO", "DisGeNet"],
+    )
+    request.cls.datadir = datadir
 
-    @classmethod
-    def tearDownClass(cls):
-        shutil.rmtree(cls.tempdir)
 
-    @pytest.mark.dependency()
-    def test_download(self):
-        geneplexus.download_select_data(
-            self.tempdir,
-            tasks="All",
-            networks="BioGRID",
-            features="Embedding",
-            GSCs="GO",
-        )
+@pytest.mark.usefixtures("data")
+class TestGenePlexusPipeline(unittest.TestCase):
+    def setUp(self):
+        self.gp = geneplexus.GenePlexus(self.datadir)
 
+    def test_filenames(self):
         for filename in FILENAMES:
-            self.assertTrue(osp.isfile(osp.join(self.tempdir, filename)))
+            self.assertTrue(osp.isfile(osp.join(self.datadir, filename)))
 
-    @pytest.mark.dependency(depends=["TestGenePlexus::test_download"])
     def test_pipeline(self):
-        myclass = geneplexus.GenePlexus(self.tempdir)
-
         # TODO: make a helper for this
         with open(osp.join(HOMEDIR, "input_genes.txt"), "r") as f:
             input_genes = [i.strip("'") for i in f.read().split(", ")]
-        myclass.load_genes(input_genes)
+        self.gp.load_genes(input_genes)
 
-        df_convert_out = myclass.convert_to_Entrez()
+        df_convert_out = self.gp.convert_to_Entrez()
         df_convert_out[["Original ID", "Entrez ID"]] = df_convert_out[["Original ID", "Entrez ID"]].astype(int)
         df_convert_out_expected = pd.read_csv(
             osp.join(HOMEDIR, "test", "expected_result", "df_convert_out.tsv"),
@@ -75,9 +77,9 @@ class TestGenePlexus(unittest.TestCase):
             df_convert_out_expected.values.tolist(),
         )
 
-        myclass.set_params("BioGRID", "Embedding", "GO")
-        pos_genes_in_net, negative_genes, net_genes = myclass.get_pos_and_neg_genes()
-        mdl_weights, df_probs, avgps = myclass.fit_and_predict()
+        self.gp.set_params("BioGRID", "Embedding", "GO")
+        pos_genes_in_net, negative_genes, net_genes = self.gp.get_pos_and_neg_genes()
+        mdl_weights, df_probs, avgps = self.gp.fit_and_predict()
 
 
 if __name__ == "__main__":
