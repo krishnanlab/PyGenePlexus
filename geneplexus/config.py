98fecae7f54e2402bbabf0f40d2a9387d13ad08d
Remy <liurenmi@msu.edu>
2022-02-27 15:19:52 -0500

    Refactor global variables

---
+++ geneplexus/config.py
@@ -0,0 +1,7 @@
+"""Global variables used by the GenePlexus library."""
+ALL_TASKS = ["IDconversion", "MachineLearning", "Similarities", "NetworkGraph"]
+ALL_NETWORKS = ["BioGRID", "STRING", "STRING-EXP", "GIANT-TN"]
+ALL_FEATURES = ["Adjacency", "Embedding", "Influence"]
+ALL_GSCS = ["GO", "DisGeNet"]
+
+URL_AZURE = f"https://mancusogeneplexusstorage.blob.core.windows.net/mancusoblob2/"
