65eabfea67c09bec8e6f7e2a764409ec89a78ace
Remy <liurenmi@msu.edu>
2022-03-12 23:35:35 -0500

    Ignore E402 in __init__, prevent cyclic imports

--- geneplexus/__init__.py
+++ geneplexus/__init__.py
@@ -2,9 +2,9 @@ from .logger_util import make_logger  # noreorder
 
 logger = make_logger("INFO")
 
-from . import download
-from . import util
-from .geneplexus import GenePlexus
+from . import download  # noqa: E402
+from . import util  # noqa: E402
+from .geneplexus import GenePlexus  # noqa: E402
 
 
 __all__ = ["download", "GenePlexus", "util"]
