8c14af051438948e9ff56b0b296cf0198c1fa17c
RemyLau <remylau961@gmail.com>
2022-03-15 08:49:18 -0400

    Nameko theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+import sphinx_nameko_theme
+html_theme = 'nameko'
+html_theme_path = [sphinx_nameko_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
