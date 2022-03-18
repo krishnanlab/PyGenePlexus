b3053f9df653e94c1921e92e6453e370095196fe
RemyLau <remylau961@gmail.com>
2022-03-18 17:12:28 -0400

    Add autodoc settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -70,6 +70,9 @@ napoleon_include_init_with_doc = True
 #
 html_theme = 'sphinx_rtd_theme'
 
+autodoc_member_order = 'alphabetical'
+autoclass_content = 'both'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
