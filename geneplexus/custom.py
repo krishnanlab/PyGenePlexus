b931654170371ce172376ebd09e1d4f691078ed8
Remy Liu <36778645+RemyLau@users.noreply.github.com>
2022-03-22 20:32:00 -0400

    Custom docs and minor fixes (#59)
    * Add doc page for custom module
    
    * Fix variable name: file_loc -> edgelist_loc
    
    * Use fstring
    
    * Use logger for printing
    
    * Fix typo
    
    * Prevent overwriting line with different types, mypy was not happy about that :(
    
    * Rename nodelist to nodeset
    
    * Minor format change, add beta kwarg
    
    * Simplify adj mat edge filling
    
    * Update Zenodo DOI

--- geneplexus/custom.py
+++ geneplexus/custom.py
@@ -1,15 +1,26 @@
 import json
 import os
+import os.path as osp
 
 import numpy as np
 
+from ._config import logger
 
-def edgelist_to_nodeorder(edgelist_loc, data_dir, net_name, sep="\t", skiplines=0):
-    """
-    The edgelist file needs to be two or three columns
-    The first two columns being the edges
-    The third column is assumed to be the edge weight if it exsits
-    If not supplying custom GSC, the file needs to be in Entrez ID space
+
+def edgelist_to_nodeorder(
+    edgelist_loc: str,
+    data_dir: str,
+    net_name: str,
+    sep: str = "\t",
+    skiplines: int = 0,
+):
+    """Convert edge list to node order.
+
+    Note:
+        The edgelist file needs to be two or three columns. The first two
+        columns being the edges. The third column is assumed to be the edge
+        weight if it exsits. If not supplying custom GSC, the file needs to be
+        in Entrez ID space.
 
     Args:
         edgelist_loc: Location of the edgelist
@@ -17,29 +28,39 @@ def edgelist_to_nodeorder(edgelist_loc, data_dir, net_name, sep="\t", skiplines=
         net_name: The name of the network
         sep: The separation used in the edgelist file (default tab)
         skiplines: The number of lines to skip for header
+
     """
-    print("Making the NodeOrder File")
-    with open(file_loc, "r") as f:
-        nodelist = set()
+    logger.info("Making the NodeOrder File")
+    with open(edgelist_loc, "r") as f:
+        nodeset = set()
         for idx, line in enumerate(f):
             if idx - skiplines < 0:
                 continue
             else:
-                line = line.strip().split(sep)
-                nodelist.add(line[0])
-                nodelist.add(line[1])
-    print("Saving NodeOrder file")
-    np.savetxt(data_dir + "NodeOrder_%s.txt" % net_name, list(nodelist), fmt="%s")
+                nodeset.update(line.strip().split(sep)[:2])
+    outfile = osp.join(data_dir, f"NodeOrder_{net_name}.txt")
+    logger.info(f"Saving NodeOrder file to {outfile}")
+    np.savetxt(outfile, list(nodeset), fmt="%s")
 
 
-def edgelist_to_matrix(edgelist_loc, nodeorder_loc, data_dir, name_name, features, sep="\t", skiplines=0):
-    """
-    The edgelist file needs to be two or three columns
-    The first two columns being the edges
-    The third column is assumed to be the edge weight if it exsits
-    If not supplying custom GSC, the file needs to be in Entrez ID space
+def edgelist_to_matrix(
+    edgelist_loc: str,
+    nodeorder_loc: str,
+    data_dir: str,
+    net_name: str,
+    features: str,
+    beta: float = 0.85,
+    sep: str = "\t",
+    skiplines: int = 0,
+):
+    """Convert edge list to adjacency matrix.
 
-    The NodeOrder file needs to be a single column text file
+    Note:
+        The edgelist file needs to be two or three columns. The first two
+        columns being the edges. The third column is assumed to be the edge
+        weight if it exsits. If not supplying custom GSC, the file needs to be
+        in Entrez ID space. Finally, the NodeOrder file needs to be a single
+        column text file.
 
     Args:
         edgelist_loc: Location of the edgelist
@@ -47,65 +68,78 @@ def edgelist_to_matrix(edgelist_loc, nodeorder_loc, data_dir, name_name, feature
         data_dir: The directory to save the file
         net_name: The name of the network
         features: Features for the networks (Adjacency or Influence, All)
+        beta: Restart parameter.
         sep: The separation used in the edgelist file (default tab)
         skiplines: The number of lines to skip for header
+
     """
-    # load in the NodeOrder file
+    if beta < 0 or beta > 1:
+        raise ValueError(f"Restart parameter (beta) must be between 0 and 1, got {beta!r}")
+
+    # Load in the NodeOrder file and make node index map
     nodelist = np.loadtxt(nodeorder_loc, dtype=str)
-    # make node to ind dict
     node_to_ind = {}
     for idx, anode in enumerate(nodelist):
         node_to_ind[anode] = idx
-    # make adjacency matrix
-    print("Making the adjacency matrix")
+
+    # Make adjacency matrix
+    logger.info("Making the adjacency matrix")
     adj_mat = np.zeros((len(nodelist), len(nodelist)), dtype=float)
     with open(edgelist_loc, "r") as f:
         for idx, line in enumerate(f):
             if idx - skiplines < 0:
                 continue
-            line = line.strip().split(sep)
-            if (line[0] not in node_to_ind) or (line[1] not in node_to_ind):
-                raise KeyError("Nodes in Edgelist not in NodeOrder file")
-            if len(line) == 2:
-                adj_mat[node_to_ind[line[0]], node_to_ind[line[1]]] = 1.0
-                adj_mat[node_to_ind[line[1]], node_to_ind[line[0]]] = 1.0
-            elif len(line) == 3:
-                adj_mat[node_to_ind[line[0]], node_to_ind[line[1]]] = float(line[2])
-                adj_mat[node_to_ind[line[1]], node_to_ind[line[0]]] = float(line[2])
-            else:
+            terms = line.strip().split(sep)
+            node1, node2 = terms[:2]
+            if len(terms) > 3:
                 raise ValueError("Too many columns in edgelist file")
+            if (node1 not in node_to_ind) or (node2 not in node_to_ind):
+                raise KeyError(f"Nodes in Edgelist but not in NodeOrder file ({node1!r} or {node2!r})")
+            i, j = node_to_ind[node1], node_to_ind[node2]
+            weight = 1.0 if len(terms) == 2 else terms[2]
+            adj_mat[i, j] = adj_mat[j, i] = weight
+
+    # Optionally make influence matrix
     if (features == "Influence") or (features == "All"):
-        print("Making the influence matrix")
-        # make influence matrix
+        logger.info("Making the influence matrix")
         adj_mat_norm = adj_mat / adj_mat.sum(axis=0)
         id_mat = np.identity(len(nodelist))
-        F_mat = 0.85 * sla.inv(id_mat - (1 - 0.85) * adj_mat_norm)
-    # save the data
-    print("Saving the data")
+        F_mat = beta * np.linalg.inv(id_mat - (1 - beta) * adj_mat_norm)
+
+    # Save the data
+    logger.info("Saving the data")
     if (features == "Adjacency") or (features == "All"):
-        np.save(data_dir + "Data_Adjacency_%s.npy" % net_name, adj_mat)
+        np.save(osp.join(data_dir, f"Data_Adjacency_{net_name}.npy"), adj_mat)
     if (features == "Influence") or (features == "All"):
-        np.save(data_dir + "Data_Influence_%s.npy" % net_name, F_mat)
+        np.save(osp.join(data_dir, f"Data_Influence_{net_name}.npy"), F_mat)
 
 
-def subset_GSC_to_network(nodeorder_loc, data_dir, GSC_name):
-    """
-    Can use download_select_data function to get preprocessed GO and DisGeNet files
+def subset_GSC_to_network(
+    nodeorder_loc: str,
+    data_dir: str,
+    GSC_name: str,
+):
+    """Subset geneset collection using network genes.
 
-    The NodeOrder file needs to be a single column text file
-    If not supplying custom GSC, the file needs to be in Entrez ID space
+    Note:
+        Use the :meth:`geneplexus.download.download_select_data` function to
+        get the preprocessed GO and DisGeNet files first.
+
+        The NodeOrder file needs to be a single column text file. If not
+        supplying custom GSC, the file needs to be in Entrez ID space.
 
     Args:
         nodeorder_loc: Location of the NodeOrder file
         data_dir: The directory to save the file
         GSC_name: The name of the GSC
+
     """
-    print("Subsetting the GSC")
-    print("This make take a few minutes")
+    logger.info("Subsetting the GSC")
+    logger.info("This make take a few minutes")
     # load in the NodeOrder file
     nodelist = np.loadtxt(nodeorder_loc, dtype=str)
     # load the orginal GSC
-    with open(data_dir + "GSCOriginal_%s.json" % GSC_name, "r") as handle:
+    with open(osp.join(data_dir, f"GSCOriginal_{GSC_name}.json"), "r") as handle:
         GSCorg = json.load(handle)
     # subset GSc based on network
     universe_genes = np.array([])
@@ -116,8 +150,8 @@ def subset_GSC_to_network(nodeorder_loc, data_dir, GSC_name):
         if (len(genes_tmp) <= 200) and (len(genes_tmp) >= 10):
             GSCsubset[akey] = {"Name": GSCorg[akey]["Name"], "Genes": genes_tmp.tolist()}
             universe_genes = np.union1d(universe_genes, genes_tmp)
-    print("Saving the data")
+    logger.info("Saving the data")
     net_name = os.path.basename(nodeorder_loc).split("_")[1].split(".t")[0]
-    with open(data_dir + "GSC_{}_{}_GoodSets.json".format(GSC_name, net_name), "w") as f:
+    with open(osp.join(data_dir, f"GSC_{GSC_name}_{net_name}_GoodSets.json"), "w") as f:
         json.dump(GSCsubset, f, ensure_ascii=False, indent=4)
-    np.savetxt(data_dir + "GSC_{}_{}_universe.txt".format(GSC_name, net_name), universe_genes, fmt="%s")
+    np.savetxt(osp.join(data_dir, f"GSC_{GSC_name}_{net_name}_universe.txt"), universe_genes, fmt="%s")
