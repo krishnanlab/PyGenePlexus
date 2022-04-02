85187ba9a0309a082e60f29e58cdaee0e5f7111f
Remy Liu <36778645+RemyLau@users.noreply.github.com>
2022-04-02 11:59:48 -0400

    Warn overwriting files (#96)
    * Add overwritting option, prompt for overwriting acknowledgement
    
    * Refactor _ack_overwrite, check zip overwrite
    
    * Add timeout to prevent indefinite waiting for overwriting acknowledgement
    
    * Test timeout

--- test/test_util.py
+++ test/test_util.py
@@ -1,8 +1,10 @@
 import os.path as osp
 import shutil
 import tempfile
+import time
 import unittest
 
+import pytest
 from parameterized import parameterized
 
 from geneplexus import util
@@ -36,6 +38,22 @@ class TestReadGeneList(unittest.TestCase):
 
         lst = util.read_gene_list(file_path, delimiter_arg)
         self.assertEqual(lst, self.lst)
+
+
+def test_timeout():
+    @util.timeout(5)
+    def wait():
+        time.sleep(0.1)
+
+    wait()
+
+    with pytest.raises(TimeoutError):
+
+        @util.timeout(1)
+        def wait():
+            time.sleep(2)
+
+        wait()
 
 
 if __name__ == "__main__":
