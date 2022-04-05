259927d1e9fb2041e25482dcfee1557f31d36423
Remy Liu <36778645+RemyLau@users.noreply.github.com>
2022-04-05 14:02:43 -0400

    Patch cross validation (#110)
    * Remove check-added-large-files hook
    
    * Save cross validation results, closes #84
    
    * Add cross validation options, closes #86
    - minimum number of positive genes
    - number of cv folds
    - null values to fill when cv not performed
    - random state for cv splits
    
    * Test cv options

--- geneplexus/_geneplexus.py
+++ geneplexus/_geneplexus.py
@@ -101,6 +101,10 @@ def _run_SL(
     negative_genes,
     net_genes,
     logreg_kwargs: Optional[Dict[str, Any]] = None,
+    min_num_pos: int = 15,
+    num_folds: int = 3,
+    null_val: float = -10,
+    random_state: Optional[int] = 0,
 ):
     if logreg_kwargs is None:
         logreg_kwargs = DEFAULT_LOGREG_KWARGS
@@ -120,15 +124,16 @@ def _run_SL(
     mdl_weights = np.squeeze(clf.coef_)
     probs = clf.predict_proba(data)[:, 1]
 
-    if len(pos_genes_in_net) < 15:
+    if len(pos_genes_in_net) < min_num_pos:
         logger.warning(
-            "Not enough genes supplied for cross validation ({len(pos_genes_in_net)}), skipping cross validation.",
+            "Insufficient number of positive genes for cross validation: "
+            f"{len(pos_genes_in_net)} ({min_num_pos} needed). Skipping cross "
+            f"validation and fill with null values {null_val}",
         )
-        avgps = [-10, -10, -10]
+        avgps = [null_val] * num_folds
     else:
         avgps = []
-        n_folds = 3
-        skf = StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=None)
+        skf = StratifiedKFold(n_splits=num_folds, shuffle=True, random_state=random_state)
         for trn_inds, tst_inds in skf.split(Xdata, ydata):
             clf_cv = LogisticRegression(**logreg_kwargs)
             clf_cv.fit(Xdata[trn_inds], ydata[trn_inds])
--- test/conftest.py
+++ test/conftest.py
@@ -11,6 +11,7 @@ def data(request):
     pytest.DATADIR = request.config.cache.makedir("download")
     pytest.HOMEDIR = pathlib.Path(__file__).parent.parent
     pytest.ANSWERDIR = osp.join(pytest.HOMEDIR, "test", "expected_result")
+    pytest.GENELIST_PATH = osp.join(pytest.HOMEDIR, "example", "input_genes.txt")
     geneplexus.download.download_select_data(
         pytest.DATADIR,
         "All",
