3e2ebce565d8a640e6db82afe31cd1bcde4678b7
RemyLau <remylau961@gmail.com>
2022-04-06 10:42:54 -0400

    Create DownloadError

--- geneplexus/exception.py
+++ geneplexus/exception.py
@@ -3,3 +3,7 @@
 
 class CustomNetworkError(Exception):
     """Raised when custom network data files not set up correctly."""
+
+
+class DownloadError(Exception):
+    """Raised when failed to download from URL within max number of retries."""
