ecdd9a5b8ef5674a99233c2ad79ae731c4269004
Remy Liu <36778645+RemyLau@users.noreply.github.com>
2022-04-06 08:55:48 -0700

    Max download retry (#115)
    * Create DownloadError
    
    * Set max retries instead of indefinitely retrying, closes #113
    
    * Move FILENAMES to conftest and make it a data fixture
    
    * Test retries

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -10,35 +10,6 @@ import yaml
 import geneplexus
 
 
-FILENAMES = [
-    "CorrectionMatrix_DisGeNet_DisGeNet_BioGRID_Embedding.npy",
-    "CorrectionMatrix_DisGeNet_GO_BioGRID_Embedding.npy",
-    "CorrectionMatrix_GO_DisGeNet_BioGRID_Embedding.npy",
-    "CorrectionMatrix_GO_GO_BioGRID_Embedding.npy",
-    "CorrectionMatrixOrder_DisGeNet_BioGRID.txt",
-    "CorrectionMatrixOrder_GO_BioGRID.txt",
-    "Data_Embedding_BioGRID.npy",
-    "Edgelist_BioGRID.edg",
-    "GSC_DisGeNet_BioGRID_GoodSets.json",
-    "GSC_DisGeNet_BioGRID_universe.txt",
-    "GSC_GO_BioGRID_GoodSets.json",
-    "GSC_GO_BioGRID_universe.txt",
-    "IDconversion_Homo-sapiens_ENSG-to-Entrez.json",
-    "IDconversion_Homo-sapiens_ENSP-to-Entrez.json",
-    "IDconversion_Homo-sapiens_ENST-to-Entrez.json",
-    "IDconversion_Homo-sapiens_Entrez-to-ENSG.json",
-    "IDconversion_Homo-sapiens_Entrez-to-Name.json",
-    "IDconversion_Homo-sapiens_Entrez-to-Symbol.json",
-    "IDconversion_Homo-sapiens_Symbol-to-Entrez.json",
-    "NodeOrder_BioGRID.txt",
-    "NodeOrder_GIANT-TN.txt",
-    "NodeOrder_STRING-EXP.txt",
-    "NodeOrder_STRING.txt",
-    "PreTrainedWeights_DisGeNet_BioGRID_Embedding.json",
-    "PreTrainedWeights_GO_BioGRID_Embedding.json",
-]
-
-
 @pytest.mark.usefixtures("data")
 def test_download_exist(caplog):
     geneplexus.download.download_select_data(
@@ -65,7 +36,7 @@ class TestGenePlexusPipeline(unittest.TestCase):
 
     @pytest.mark.order(0)
     def test_filenames(self):
-        for filename in FILENAMES:
+        for filename in pytest.FILENAMES:
             with self.subTest(filename=filename):
                 self.assertTrue(osp.isfile(osp.join(pytest.DATADIR, filename)))
 
