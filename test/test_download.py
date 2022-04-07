95f848315f42d4a6dc67d23182f7644476d0b639
Remy <liurenmi@msu.edu>
2022-04-07 12:26:27 -0400

    Disable download warning in test raise

--- test/test_download.py
+++ test/test_download.py
@@ -5,7 +5,9 @@ from urllib.parse import urljoin
 import pytest
 
 from geneplexus import download
+from geneplexus._config import logger
 from geneplexus._config.config import URL_DATA
+from geneplexus._config.logger_util import log_level_context
 from geneplexus.exception import DownloadError
 
 
@@ -14,6 +16,7 @@ def test_retries(requests_mock, tmpdir):
     for filename in pytest.FILENAMES:
         requests_mock.get(urljoin(URL_DATA, f"{filename}.zip"), status_code=429)
 
-    with pytest.raises(DownloadError) as excinfo:
-        download._download_from_url(tmpdir, pytest.FILENAMES)
+    with log_level_context(logger, "CRITICAL"):
+        with pytest.raises(DownloadError) as excinfo:
+            download._download_from_url(tmpdir, pytest.FILENAMES)
     assert str(excinfo.value) == "Failed to download all required files (MAX_RETRY=10)"
