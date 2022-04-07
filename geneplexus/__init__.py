127c0582c4c1ea2cee2102b8c33a404218a6603b
RemyLau <remylau961@gmail.com>
2022-04-07 07:01:13 -0400

    Add packge overview

--- geneplexus/__init__.py
+++ geneplexus/__init__.py
@@ -1,4 +1,13 @@
-"""The GenePlexus package."""
+"""A Python package for GenePlexus.
+
+[GenePlexus]_ is a network based supervised learning method for gene
+classification. Given a list of input genes and a selection of background gene
+set collection (:term:`GSC`), it trains a logistic regression model using one
+of three network derived features (:term:`adjacency`, :term:`influence`, or
+:term:`embedding`) and generate genome wide predictions about genes that are
+functionally similar to the input gene list.
+
+"""
 from ._config import config  # noreorder
 from . import download
 from . import util
