4182c803ddec2fb2a9b275964c35a30c409fae41
RemyLau <remylau961@gmail.com>
2022-06-14 07:17:04 -0400

    Fix linewidth

--- geneplexus/__init__.py
+++ geneplexus/__init__.py
@@ -1,5 +1,4 @@
-"""Welcome to PyGenePlexus! PyGenePlexus is a python package version
-for running the [GenePlexus]_ model
+"""PyGenePlexus is a Python package for running the [GenePlexus]_ model.
 
 .. currentmodule:: geneplexus.GenePlexus
 
@@ -16,27 +15,29 @@ molecular interaction networks.
 
 Given a list of input genes and a geneset collection (:term:`GSC`) to help
 select negative examples, the package trains a logistic regression model
-using one of three network derived features (:term:`adjacency`, :term:`influence`, or
-:term:`embedding`) and generates the following outputs
+using one of three network derived features (:term:`adjacency`,
+:term:`influence`, or :term:`embedding`) and generates the following outputs
 
-#. Genome-wide prediction of how **functionally similar** a gene is \
-to the input gene list. See :meth:`fit_and_predict`
-#. (Optional) Interpretability of the model is provided by \
-comparing the model trained on the user gene set to \
-models pretrained on 1000's of known gene sets from [GO]_ bioloigcal \
-proceses and [DisGeNet]_ diseases. See :meth:`make_sim_dfs`
-#. (Optional) Interpretability of the top predicted genes is \
-provided by returning their network connectivity. \
-:meth:`make_small_edgelist`
+#. Genome-wide prediction of how **functionally similar** a gene is  to the \
+input gene list. See :meth:`fit_and_predict`
+#. (Optional) Interpretability of the model is provided by comparing the \
+model trained on the user gene set to models pretrained on 1000's of known \
+gene sets from [GO]_ bioloigcal proceses and [DisGeNet]_ diseases. See \
+:meth:`make_sim_dfs`
+#. (Optional) Interpretability of the top predicted genes is provided by \
+returning their network connectivity. :meth:`make_small_edgelist`
 
 .. note::
 
     **Links to other GenePlexus products**
 
     * `GenePlexus Web Server <https://www.geneplexus.net>`_
-    * `GenePlexus Web Server Paper <https://academic.oup.com/nar/advance-article/doi/10.1093/nar/gkac335/6586869?login=true>`_
-    * `GenePlexus Benchmarking Paper <https://academic.oup.com/bioinformatics/article/36/11/3457/5780279>`_
-    * `GenePlexus Benchmarking Paper Repo <https://github.com/krishnanlab/GenePlexus>`_
+    * `GenePlexus Web Server Paper \
+<https://academic.oup.com/nar/advance-article/doi/10.1093/nar/gkac335/6586869?login=true>`_
+    * `GenePlexus Benchmarking Paper \
+<https://academic.oup.com/bioinformatics/article/36/11/3457/5780279>`_
+    * `GenePlexus Benchmarking Paper Repo \
+<https://github.com/krishnanlab/GenePlexus>`_
     * `PyGenePlexus PyPi Package <https://pypi.org/project/geneplexus/>`_
     * `PyGenePlexus GitHub Repo <https://github.com/krishnanlab/PyGenePlexus>`_
 
@@ -53,9 +54,10 @@ pip and run a quick example as follows.
     geneplexus -i my_gene_list.txt --output_dir my_result
 
 Note that you need to supply the ``my_gene_list.txt`` file, which is a line
-separated gene list text file  (NCBI Entrez IDs, Symbol or Ensembl IDs are accepted).
-An example can be found on the `GitHub page <https://github.com/krishnanlab/PyGenePlexus>`_
-under ``example/input_genes.txt``. More info can be found in :ref:`PyGenePlexus CLI`.
+separated gene list text file  (NCBI Entrez IDs, Symbol or Ensembl IDs are
+accepted). An example can be found on the
+`GitHub page <https://github.com/krishnanlab/PyGenePlexus>`_ under
+``example/input_genes.txt``. More info can be found in :ref:`PyGenePlexus CLI`.
 
 .. warning::
 
