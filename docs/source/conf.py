b75613d44b40fed2e90f51248c3675901325c3f3
ChristopherMancuso <chrismancuso1984@gmail.com>
2024-12-18 00:22:28 -0500

    updated package version in places

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, Krishnan Laboratory at Univeristy of Colorado Anschutz Medica
 author = 'Remy Liu and Christopher A. Mancuso'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0-dev'
+release = '2.0.1'
 
 
 # -- General configuration ---------------------------------------------------
