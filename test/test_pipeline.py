b0c4dcac4160ec18889555987bd557cdb9480caf
ChristopherMancuso <chrismancuso1984@gmail.com>
2025-09-23 16:39:55 -0400

    pipeline test now sets -sr to Mouse for cli

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -166,7 +166,7 @@ class TestGenePlexusPipeline(unittest.TestCase):
 
     @pytest.mark.order(7)
     def test_cli_main(self):
-        os.system(f"geneplexus -i {pytest.GENELIST_PATH} -fl {pytest.DATADIR} -od {pytest.CLIRESULTSDIR} --overwrite")
+        os.system(f"geneplexus -i {pytest.GENELIST_PATH} -fl {pytest.DATADIR} -od {pytest.CLIRESULTSDIR} -sr Mouse --overwrite")
 
         assert os.path.isfile(osp.join(pytest.CLIRESULTSDIR, "top_level_config.json"))
 
